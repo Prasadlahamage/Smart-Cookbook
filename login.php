e76322040ef2361d12106d902a51595c78fcd9ba
Prasad lahamage <52752672+Prasadlahamage@users.noreply.github.com>
2022-04-08 00:16:17 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,56 @@
-<!DOCTYPE html>
 <html>
     <head>
-        <title>Admin Login</title>
+        <title>
+            Home
+            
+        </title>
         <link rel="stylesheet" href="style.css">
+
     </head>
     <body>
-        <h2 id="heading">Login As Administrator</h2>
-        <div id="frm">
-            <form action="validate.php" method="POST">
-                <p>
-                    <label><b>Username : </label>
-                    <input placeholder="username" type="text" name="username" id="username"required/>
-               </p>
-               <p>
-                    <label>Password : </label>
-                    <input placeholder="password" type="password" name="password" id="password" required/>
-               </p>
-               <p>
-                    <input type="submit" id="btn" value="Login" />
-                    <input type="reset" id="rst"value="Reset"/><br>
-                    <a href="index.php" class="login">get back to user</a>
-               </p>
-            </form>
+        <form action="" method="POST">
+            <input type="text" name="main_ingredient" placeholder="Enter the ingredient that you have "/> <br/>
+            <input type="submit" name="search" value="Search Recipee" >
+        </form>
+        <?php
+		error_reporting(0);
+            session_start();
+            $con=mysqli_connect('localhost','root','','recipe') or die("Error");
+            
+            if(isset($_POST['search'])){
+                $MAIN_INGREDIENT = $_POST['main_ingredient'];
+
+                $query = "SELECT DISH_NAME,METHOD,INGREDIENTS FROM RECIPE WHERE MAIN_INGREDIENT ='$MAIN_INGREDIENT'";
+            
+                
+                $query_run = mysqli_query($con,$query);
+                while ($row = mysqli_fetch_array($query_run)) {   
+                                     
+                    ?>
+                    <details>
+                        <summary><button name="DISH_NAME">
+                            <?php echo $row['DISH_NAME'];?>
+                            </summary>
+                             </button>
+                            <b>INGREDIENTS:
+                            <h3> <?php echo $row['INGREDIENTS']?><br>
+                       RECIPE:
+                         <h3><?php  echo $row['METHOD'];?></b>
+                         
+                         
+                         </details>
+                        <br>
+                    <?php
+                }
+            }
+            
+        ?>
+
+        <div class="login">
+            <a href="index.php">
+                Login as Administrator
+            </a>
         </div>
+
     </body>
 </html>
